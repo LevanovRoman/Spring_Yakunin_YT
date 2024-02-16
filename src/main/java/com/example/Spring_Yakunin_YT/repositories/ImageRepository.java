ea994ed16f043c0f575736c3d500e022e9063ab3
levanovroman <levanovroman2016@yandex.ru>
2024-02-16 12:25:04 +0300

    image

---
+++ src/main/java/com/example/Spring_Yakunin_YT/repositories/ImageRepository.java
@@ -0,0 +1,7 @@
+package com.example.Spring_Yakunin_YT.repositories;
+
+import com.example.Spring_Yakunin_YT.models.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+}
